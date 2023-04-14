f4d4e33d8321850aabca6ba174b351c035fc999b
PKurianski <pedrokurianski@gmail.com>
2023-04-14 18:26:49 -0300

    final

--- Bootcamp/src/App.java
+++ Bootcamp/src/App.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import classes.Bootcamp;
 import classes.Curso;
+import classes.Dev;
 import classes.Mentoria;
 
 public class App {
@@ -20,8 +22,34 @@ public class App {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcmap Java Developer");
+        bootcamp.setDescricao("Descroção Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devKurianski = new Dev();
+        devKurianski.setNome("Pedro");
+        devKurianski.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Pedro Kurianski:" + devKurianski.getConteudosInscritos());
+        devKurianski.progredir();
+        devKurianski.progredir();
+        System.out.println("Conteúdos Inscritos Pedro Kurianski:" + devKurianski.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Pedro Kurianski:" + devKurianski.getConteudosConcluidos());
+        System.out.println("XP:" + devKurianski.calcularTotalXp());
+
+        Dev devAlves = new Dev();
+        devAlves.setNome("Henrique");
+        devAlves.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Henrique Alves:" + devAlves.getConteudosInscritos());
+        devAlves.progredir();
+        System.out.println("Conteúdos Inscritos Henrique Alves:" + devAlves.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Henrique Alves:" + devAlves.getConteudosConcluidos());
+        System.out.println("XP:" + devAlves.calcularTotalXp());
     }
 }
